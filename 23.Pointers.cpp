0efd734a9889f496d2e7ceef252fbe37c4af6b76
Ankush Debnath <ankushdebnath680@gmail.com>
2023-07-14 23:38:42 +0530

    commit

--- 23.Pointers.cpp
+++ 23.Pointers.cpp
@@ -6,9 +6,23 @@ int main()
     // Symbol Table is an important data structure created and maintained by the compiler in order to keep track of semantics of variables i.e. it stores information about the scope and binding information about names, information about instances of various entities such as variable and function names, classes, objects, etc.
     int a = 10;
     int *p = &a;
-    cout << "Address of a: " << &a << " = " << p << endl;
-    cout << "Value of a: " << a << " = " << *p << endl;
+    cout << "Address of a / Value of p: " << &a << " = " << p << endl;
+    cout << "Value of a / Value of p (as a pointer): " << a << " = " << *p << endl;
     cout << "Address of p: " << &p << endl;
     cout << "Size of p (address): " << sizeof(p) << endl;
     cout << "Size of *p (int): " << sizeof(*p) << endl;
+
+    // copy pointer
+    int *q = p;
+    cout << "Value of a (via q): " << *q << endl;
+
+    // pointer increment
+    cout << "value of q: " << q << endl;
+    cout << "value of q after incrementing (+ size of datatype): " << ++q << endl;
+
+    // pointer to pointer
+    int **r = &p;
+    cout << "value of r: " << r << endl;
+    cout << "value of r (as a pointer): " << *r << endl;
+    cout << "value of r (as a pointer of pointer): " << **r << endl;
 }
