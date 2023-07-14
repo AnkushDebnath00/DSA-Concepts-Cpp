92e5559b92ba9e472b9adb94c5bffe338b861580
Ankush Debnath <ankushdebnath680@gmail.com>
2023-07-14 15:13:20 +0530

    commit

--- 22.BasicMaths.cpp
+++ 22.BasicMaths.cpp
@@ -69,16 +69,36 @@ int lcm(int a, int b)
     return (a * b) / gcd(a, b);
 }
 
+// Modulo Operations
+// (a+b)%m = ((a%m)+(b%m))%m
+// (a-b)%m = ((a%m)-(b%m)+m)%m
+// (a*b)%m = ((a%m)*(b%m))%m
+// (a/b)%m = ((a%m)*(b^-1%m))%m
+// (a^b)%m = ((a%m)^b)%m
+
+int fastExponentiation(int m, int n)
+{
+    // m^n
+    // Time Complexity: O(log(n))
+    if (n == 0)
+        return 1;
+    if (n % 2 == 0)
+        return fastExponentiation(m * m, n / 2);
+    return m * fastExponentiation(m * m, (n - 1) / 2);
+}
+
 int main()
 {
-    int m, n;
-    cout << "Enter the 2 numbers: ";
-    cin >> m;
+    int a, b, m, n;
+    cout << "Enter the 1 number: ";
     cin >> n;
     cout << "is Prime? " << isPrime(n) << endl;
     cout << "Number of primes: " << countPrimes(n) << endl;
     cout << "Number of primes: " << countPrimesOptimized(n) << endl;
     cout << "Number of primes: " << countPrimesMoreOptimized(n) << endl;
+    cout << "Enter the 2 numbers: ";
+    cin >> m;
+    cin >> n;
     cout << "Greatest Common Dividend: " << gcd(m, n) << endl;
     cout << "Least Common Multiple: " << lcm(m, n) << endl;
 }
